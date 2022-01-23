d8c6981d9181f5bd9959a141068965acbdf62667
Ajay <ajaysajikumar8@gmail.com>
2022-01-23 23:26:51 +0530

    Bug fix

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for x in question_data:
-    question_bank.append(Question(x["text"], x["answer"]))
+    question_bank.append(Question(x["question"], x["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,6 +3,8 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
+
 
     def still_has_questions(self):
         total_questions = len(self.question_list)
@@ -13,4 +15,17 @@ class QuizBrain:
         question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number} {question.text} (True/False)?: ")
-        print(user_answer)
+        self.check_answer(user_answer, question.answer)
+    
+
+    def check_answer(self, user_ans, correct_ans):
+        if user_ans.lower() == correct_ans.lower():
+            print("you got it right")
+            self.score += 1
+        else:
+            print("You got it wrong")
+        print(f"The correct answer was {correct_ans}")
+        print(f"Your current score is {self.score}/{self.question_number}")
+        print("\n")
+
+        
